1063a63b661bf94fa4bcca580b738327815e330d
Arthur Nakayama <61486792+ATKNK@users.noreply.github.com>
2025-08-28 11:12:49 -0300

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import io.javalin.Javalin;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Javalin app = Javalin.create().start(7770);
+
+        app.get("/tads24", ctx -> ctx.result("TADS24 é uma turma exepcionalmente legal"));
+    }
+}
