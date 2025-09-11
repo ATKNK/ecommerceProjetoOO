54210a7ecbb1885f6a6c53c1596cf3681f63ea2d
Arthur Nakayama <61486792+ATKNK@users.noreply.github.com>
2025-09-11 09:53:51 -0300

    Feat: Criação parcial das views

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,18 +1,33 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-import com.example.models.Product;
-
+import freemarker.template.Configuration;
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Javalin app = Javalin.create().start(7770);
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_34);
+        cfg.setClassForTemplateLoading(Main.class, "/templates");
+        cfg.setDefaultEncoding("UTF-8");
+
+        Javalin app = Javalin.create(config ->{
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.directory = "/public";
+                staticFiles.location = Location.CLASSPATH;
+            });
+        }).start(7070);
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("product", "Object");
+
+        app.get("/", ctx-> {
+            ctx.redirect("index.html");
+        });
 
         app.get("/tads24", ctx -> ctx.result("TADS24 é uma turma exepcionalmente legal"));
     }
